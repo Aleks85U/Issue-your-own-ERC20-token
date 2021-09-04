caca5d3923a2e26cc0b3a493e15816c57660a292
Aleks85U <90089627+Aleks85U@users.noreply.github.com>
2021-09-04 19:14:34 +0800

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'AUZ' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
-// Decimals    : 18
+// Deployed to : 0xf75D4b45Ef2975411b989991e07870D547c28859
+// Symbol      : AUZ
+// Name        : AUZAURUMS Token
+// Total supply: 1000
+// Decimals    : 3
 //
 // Enjoy.
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract AUZAURUMS is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        symbol = "AUZ";
+        name = "AUZAURUMS";
+        decimals = 3;
+        _totalSupply = 1000000;
+        balances[0xf75D4b45Ef2975411b989991e07870D547c28859] = _totalSupply;
+        emit Transfer(address(0), 0xf75D4b45Ef2975411b989991e07870D547c28859, _totalSupply);
     }
 
 
